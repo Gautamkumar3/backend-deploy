07f1c93bff11d3746aac5ad0732317012a0b001b
Gautam Kumarm <g2gautamsingh@gmail.com>
2025-04-08 23:48:21 +0530

    basic

--- src/server.js
+++ src/server.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const dbConnect = require("./config/db.js");
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
+// const dbConnect = require("./config/db.js");
 require("dotenv").config();
-const mediaRouter = require("./routes/media");
-const userRouter = require("./routes/user");
-const adminRouter = require("./routes/admin");
-const PORT = process.env.PORT || 8000;
+// const mediaRouter = require("./routes/media");
+// const userRouter = require("./routes/user");
+// const adminRouter = require("./routes/admin");
+// const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/media", mediaRouter);
-app.use("/user", userRouter);
-app.use("/admin", adminRouter);
+// app.use("/media", mediaRouter);
+// app.use("/user", userRouter);
+// app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to augmentic");
 });
 
 app.listen(PORT, async () => {
-  await dbConnect();
+  // await dbConnect();
   console.log(`Server is running on port ${PORT}`);
 });
